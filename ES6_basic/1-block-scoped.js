cc30ca41d27472825eaf743245244c5fb940fdce
Callacos <callacos74@gmail.com>
2025-03-27 14:01:50 +0100

    Refactor taskBlock function to use let for variable declaration and improve boolean parameter validation

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,14 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
-  if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+  if (typeof trueOrFalse === 'boolean') {
+    if (trueOrFalse) {
+      task = true;
+      task2 = false;
+    }
+  } else {
+    console.error('Paramètre invalide : un booléen est requis');
   }
 
   return [task, task2];
