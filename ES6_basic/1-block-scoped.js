f7ad4891b7966f59eff5e5db91d627f271345021
Callacos <callacos74@gmail.com>
2025-03-27 14:02:51 +0100

    Simplify taskBlock function by removing redundant boolean checks and improving error handling

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,13 +2,9 @@ export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
 
-  if (typeof trueOrFalse === 'boolean') {
-    if (trueOrFalse) {
-      task = true;
-      task2 = false;
-    }
-  } else {
-    console.error('Paramètre invalide : un booléen est requis');
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
