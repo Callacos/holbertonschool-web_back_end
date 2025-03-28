a3690f44e02bc4264c55d826833064176a571215
Callacos <callacos74@gmail.com>
2025-03-28 09:06:03 +0100

    Refactor import statements to remove file extensions and improve consistency

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,41 +1,40 @@
 export default class Airport {
-	constructor(name, code) {
-	  this.name = name;
-	  this.code = code;
-	}
-  
-	// Name getter and setter
-	get name() {
-	  return this._name;
-	}
-  
-	set name(value) {
-	  if (typeof value !== 'string') {
-		throw new TypeError('Name must be a string');
-	  }
-	  this._name = value;
-	}
-  
-	// Code getter and setter
-	get code() {
-	  return this._code;
-	}
-  
-	set code(value) {
-	  if (typeof value !== 'string') {
-		throw new TypeError('Code must be a string');
-	  }
-	  this._code = value;
-	}
-  
-	// Default string description returns airport code
-	toString() {
-	  return `[object ${this._code}]`;
-	}
-  
-	// Alternatively, using Symbol.toStringTag for modern JS
-	get [Symbol.toStringTag]() {
-	  return this._code;
-	}
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
   }
-  
+
+  // Name getter and setter
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  // Code getter and setter
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  // Default string description returns airport code
+  toString() {
+    return `[object ${this._code}]`;
+  }
+
+  // Alternatively, using Symbol.toStringTag for modern JS
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
