770d0de2b8a79aaf6cc413b45caf6d376e0db43f
Callacos <callacos74@gmail.com>
2025-04-02 11:07:12 +0200

    Refactor promise handling in getFullResponseFromAPI and handleResponseFromAPI for improved readability

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,14 @@
 export default function handleResponseFromAPI(promise) {
-	  return promise
-	.then((response) => {
-	  const { status, body } = response;
-	  return {
-		status: 200,
-		body: 'Success',
-	  };
-	})
-	.catch(() => new Error('The fake API is not working currently'))
-	.finally(() => {
-	  console.log('Got a response from the API');
-	});
+  return promise
+    .then((response) => {
+      const { status, body } = response;
+      return {
+        status: 200,
+        body: 'Success',
+      };
+    })
+    .catch(() => new Error('The fake API is not working currently'))
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
