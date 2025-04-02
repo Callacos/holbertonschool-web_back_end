2b9dc8cdc93a6174be681bfe169ca0af7d525e41
Callacos <callacos74@gmail.com>
2025-04-02 11:03:32 +0200

    Fix formatting and ensure proper function closure in getResponseFromAPI

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,6 +1,5 @@
 export default function getResponseFromAPI() {
-	return new Promise((resolve) => {
-	  setTimeout(() => resolve('API response'), 1000);
-	});
-  }
-  
+  return new Promise((resolve) => {
+    setTimeout(() => resolve('API response'), 1000);
+  });
+}
