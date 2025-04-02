ddecab33f1fc1fcfe690e6e41dd59ed9d4108a97
Callacos <callacos74@gmail.com>
2025-04-02 11:10:51 +0200

    Refactor handleResponseFromAPI to simplify promise handling and improve error management

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,10 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then((response) => {
-      const { status, body } = response;
-      return {
-        status: 200,
-        body: 'Success',
-      };
-    })
-    .catch(() => new Error('The fake API is not working currently'))
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
     });
