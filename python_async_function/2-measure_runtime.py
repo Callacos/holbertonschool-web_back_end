4fb49c90cde781cb072538162608f1e85009c180
Callacos <callacos74@gmail.com>
2025-04-24 14:19:53 +0200

    Update shebang to use env for better portability across environments

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """documentation for wait_n"""
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """documentation for wait_n"""
 import asyncio
 
