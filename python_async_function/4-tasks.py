3f7c4d119727461401e496f7cbbfb5b5f12c0850
Callacos <callacos74@gmail.com>
2025-04-24 13:36:08 +0200

    Fix import statement and update module-level docstring for clarity in 4-tasks.py

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""documentation for wait_n"""
 import asyncio
-task_wait_random = __import__('3-tasks').task_wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def task_wait_n(n: int, max_delay: int):
