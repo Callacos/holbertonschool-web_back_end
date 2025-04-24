cc989ace1893bcaf833b9f8b3150286a04957956
Callacos <callacos74@gmail.com>
2025-04-24 10:29:48 +0200

    Add measure_time function to calculate runtime of wait_n

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure the runtime of wait_n."""
+    import time
+    import asyncio
+
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.perf_counter()
+
+    return (end_time - start_time) / n
