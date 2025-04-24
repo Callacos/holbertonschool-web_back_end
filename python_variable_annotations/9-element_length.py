a70b9b8c638230733f4b29c4dd703ba95216a6f1
Callacos <callacos74@gmail.com>
2025-04-24 08:23:02 +0200

    Fix formatting and improve readability of docstring in element_length function

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,8 +5,10 @@ from typing import Iterable, Sequence, List, Tuple
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    Prend un itérable de séquences (comme des listes ou des chaînes de caractères) et 
-    retourne une liste de tuples. Chaque tuple contient une séquence de l'itérable 
+    Prend un itérable de séquences (comme des
+    listes ou des chaînes de caractères) et
+    retourne une liste de tuples. Chaque tuple
+     contient une séquence de l'itérable
     et sa longueur.
     """
     return [(i, len(i)) for i in lst]
