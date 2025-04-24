11026c4254d05af2d557f4a843250409557eb8b4
Callacos <callacos74@gmail.com>
2025-04-24 13:37:28 +0200

    Refactor task_wait_n function to improve readability and add return type annotation

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """documentation for wait_n"""
 import asyncio
+import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def task_wait_n(n: int, max_delay: int):
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    delays = []
-    for completed_task in asyncio.as_completed(tasks):
-        delay = await completed_task
-        delays.append(delay)
-    return delays
+async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """rock bap dooda diwap diwap diwap"""
+    tasks = []
+    for i in range(n):
+        tasks.append(wait_random(max_delay))
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
